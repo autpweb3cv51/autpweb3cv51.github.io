ec84dae09d46279d4b8807b776bb15649de5b6d4
autpweb3cv51 <94427594+autpweb3cv51@users.noreply.github.com>
2021-11-15 23:51:05 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyA6VLhd313KNRdVHosu5VUVRvZr6T9PPJQ",
-    authDomain: "autpweb.firebaseapp.com",
-    projectId: "autpweb",
-    storageBucket: "autpweb.appspot.com",
-    messagingSenderId: "629103536755",
-    appId: "1:629103536755:web:e23e8e59a949314af6d806",
-    measurementId: "G-60DTENZ39J"
+     apiKey: "AIzaSyA6VLhd313KNRdVHosu5VUVRvZr6T9PPJQ",
+  authDomain: "autpweb.firebaseapp.com",
+  projectId: "autpweb",
+  storageBucket: "autpweb.appspot.com",
+  messagingSenderId: "629103536755",
+  appId: "1:629103536755:web:e23e8e59a949314af6d806",
+  measurementId: "G-60DTENZ39J
     });
